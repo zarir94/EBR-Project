4c85e905a83f63c30d12657239ca2b2bcb924a20
S.M.Shahriar Zarir <148766170+zarir94@users.noreply.github.com>
2024-09-12 16:47:05 +0000

    up

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask_migrate import Migrate
-from flask import Flask
+from flask import Flask, request, jsonify
 from model import *
+from common import all_boards
 import os, threading
 
 app = Flask(__name__)
@@ -15,11 +16,27 @@ with app.app_context(): db.create_all()
 def index():
     return 'Hello World'
 
+@app.route('/add-to-fetch', methods=['GET', 'POST'])
+def add_entry():
+    if request.method == 'POST':
+        roll = request.form.get('roll', None, int)
+        year = request.form.get('year', None, int)
+        board = request.form.get('board', None, str)
+        if not roll: return jsonify(dict(success=False, msg='Invalid roll.'))
+        if len(str(year)) != 4: return jsonify(dict(success=False, msg='Invalid year.'))
+        if board.upper() not in all_boards: return jsonify(dict(success=False, msg='Invalid board. Should be one of ' + ','.join(all_boards)))
+        if not SSCResult.query.filter_by(roll=roll, year=year, board=board).first():
+            db.session.add(SSCResult(roll=roll, year=year, board=board))
+            db.session.commit()
+        return jsonify(success=True, msg='Record will be added.')
+    return 'Send POST with roll, year, board'
+
 
 if __name__ == '__main__':
-    t = threading.Thread(target=lambda: app.run('0.0.0.0', 8000, False))
-    t.start()
-    import data_entry
+    app.run('0.0.0.0', 8000, False)
+    # t = threading.Thread(target=lambda: app.run('0.0.0.0', 8000, False))
+    # t.start()
+    # import data_entry
     
 
 # DB UPDATE
